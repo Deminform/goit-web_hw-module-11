4c27369728da05f92eb3520ab0f65762289a4b92
Deminform <deminform@gmail.com>
2024-10-28 19:33:46 +0100

    0.1.2

--- src/schemas/note.py
+++ src/schemas/note.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel
+from pydantic import Field
 
 
 class NoteModel(BaseModel):
     name: str
     description: str
     done: bool
+
+
+class NoteResponseModel(NoteModel):
+    id: int = Field(default=1, ge=1)
