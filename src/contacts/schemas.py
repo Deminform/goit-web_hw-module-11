b959da3d442ca9f3b13feadf79344264b2d1362a
Deminform <deminform@gmail.com>
2024-11-09 20:18:00 +0100

    0.1.12

--- src/contacts/schemas.py
+++ src/contacts/schemas.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, Field, EmailStr
 class ContactSchema(BaseModel):
     first_name: str = Field(min_length=2, max_length=50)
     last_name: str = Field(min_length=2, max_length=50)
-    email: EmailStr = Field(...)
+    email: EmailStr = Field(..., max_length=50)
     phone: str = Field(max_length=10)
     birthday: date = Field(None)
     description: str = Field(None, max_length=300)
@@ -19,7 +19,7 @@ class ContactCreateSchema(ContactSchema):
 class ContactUpdateSchema(ContactSchema):
     first_name: str = Field(None, min_length=2, max_length=100)
     last_name: EmailStr = Field(None)
-    email: EmailStr = Field(None)
+    email: EmailStr = Field(None, max_length=50)
     phone: str = Field(None, max_length=10)
     birthday: date = Field(None)
     description: str = Field(None, max_length=300)
